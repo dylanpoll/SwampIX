04801afe70c111edc46e3d64ac8a70472606ef17
Dylan Poll <dylanmanpoll@gmail.com>
2023-01-28 12:34:54 -0600

    update

---
+++ public/clientSideConsts.js
@@ -0,0 +1,5 @@
+// this is for local variables that the client side pages may use, made it so it would be easy to work with.
+export const getServiceEndpoint = 'https://appwrite.devdylan.us/v1';
+export const getFileStorageBuckitID = '63d3a5caa59d18d22463';
+export const getwebsiteDomainRoot = 'https://swampix.devdylan.us/';
+export const getAppwriteProjectID = '63d3a5777ec8d871894e';
